c7aa5b2c6503a7070553ce7986cf4cb365161c2e
Gokul86356 <2021wa86356@wilp.bits-pilani.ac.in>
2025-07-14 14:24:08 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello world {
     public static void main (String[] args) {
-            system.out.println (" hello, world_2021wc86415!");
+            system.out.println (" hello, world_2021wa86356!");
             system.out.println ("commit_change1");
     }
 }
